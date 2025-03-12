281e1e27c1111eb3fd6c7bd653c49668b21b9d56
fahmi <amixcustomlinux@gmail.com>
2025-03-12 13:50:35 +0700

    [DEV] Update article

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\StorePostRequest;
+use App\Http\Requests\UpdatePostRequest;
 use App\Models\Articles;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Str;
@@ -36,11 +37,26 @@ class PostController extends Controller
             'title' => $request->title,
             'slug' => Str::slug($request->title),
             'content' => $request->content,
-            'publish_at' => $request->publish_at,
+            'publish_at' => $request->published_at ? $request->published_at : date('Y-m-d H:i:s'),
             'status' => $request->convertStatus(),
-            'author_id' => auth()->id(), // Assuming user is logged in
+            'author_id' => auth()->id()
         ]);
 
         return redirect()->route('posts.index')->with('success', 'Post created successfully!');
+    }
+
+    public function edit(Articles $post)
+    {
+        return view('posts.edit', compact('post'));
+    }
+
+    public function update(UpdatePostRequest $request, Articles $post)
+    {
+        $post->status = $request->convertStatus();
+        $post->publish_at = $request->published_at ? $request->published_at : date('Y-m-d H:i:s');
+
+        $post->update($request->validated());
+
+        return redirect()->route('posts.index')->with('success', 'Post updated successfully!');
     }
 }
